73dfb081247dd1d9975f515c212904e1d214948c
MUKESH SINGH <mmukesh35436@gmail.com>
2026-09-04 22:16:52 +0530

    Create sales_analysis database and sales table

--- sql_queries.sql
+++ sql_queries.sql
@@ -5,3 +5,15 @@
 CREATE DATABASE sales_analysis;
 
 USE sales_analysis;
+
+-- Create Sales Table
+
+CREATE TABLE sales (
+    order_id INT PRIMARY KEY,
+    product VARCHAR(50),
+    category VARCHAR(50),
+    region VARCHAR(50),
+    quantity INT,
+    unit_price DECIMAL(10,2),
+    sales DECIMAL(12,2)
+);
